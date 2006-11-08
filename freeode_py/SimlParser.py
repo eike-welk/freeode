70057aecc054f756df20e9d07dc9f1eee5fa7027
eike <>
2006-11-08 01:30:41 +0000

    Added more parse actions. Now syntax tree must be implemented.

--- freeode_py/SimlParser.py
+++ freeode_py/SimlParser.py
@@ -27,9 +27,13 @@ __doc__ = \
 """
 Parser for the SIML simulation language.
 """
+#TODO: usage (above)
 
 from pyparsing import Literal,Keyword,CaselessLiteral,Word,Combine,Group,Optional, \
-    ZeroOrMore,Forward,nums,alphas,restOfLine,ParseResults
+    ZeroOrMore,Forward,nums,alphas,restOfLine,ParseResults, ParseException, \
+    ParseFatalException
+
+
 
 
 class ParseStage(object):
@@ -42,7 +46,7 @@ class ParseStage(object):
     
     Usage:
     parser = ParseStage()
-    result = parser.parseString("0+1+2+3+4");
+    result = parser.parseString("0+1+2+3+4")
     """
     
     debugSyntax = 0 
@@ -58,9 +62,11 @@ class ParseStage(object):
     List of all keywords (filled by defineLanguageSyntax() and defineKeyword).
     """
     
+    
     def __init__(self):
         self._parser = self.defineLanguageSyntax() #Create parser object
         """The parser object from pyParsing"""
+    
     
     def defineKeyword(self, inString):
         """
@@ -72,7 +78,8 @@ class ParseStage(object):
             self.keywords.append(inString)
         return Keyword(inString)
     
-##    ------------------------------ Parse Actions ------------------------------------
+    
+#------------- Parse Actions -------------------------------------------------*
     def actionDebug(self, str, loc, toks):
         """Parse action for debuging."""
         print "------debug action"
@@ -82,9 +89,11 @@ class ParseStage(object):
         print "-------------------"
         return toks
     
+    
     def actionInfixBinOp(self, str, loc, toks):
         """
-        Parse action: Put additional information into parse result, that would 
+        Parse action for binary mathematical operations (+ - * / ^). 
+        Put additional information into parse result, that would 
         be lost otherwise. The information is stored in a dict, and put before 
         the original parse result.
         """
@@ -94,14 +103,16 @@ class ParseStage(object):
         elif self.debugSyntax == 1:
             return toks.copy()
         
-        # toks is nested like this [["2","+","5"]]
+        # toks is structured like this [["2","+","5"]]
         newToks = ParseResults([{"type":"mopI2", "loc":loc}]) #create dict
         newToks += toks[0].copy() #add original contents
         return ParseResults([newToks]) #wrap in []; return.
     
+    
     def actionPrefixUnaryOp(self, str, loc, toks):
         """
-        Parse action: Put additional information into parse result, that would 
+        Parse action for mathematical unary operations (-). 
+        Put additional information into parse result, that would 
         be lost otherwise. The information is stored in a dict, and put before 
         the original parse result.
         """
@@ -111,14 +122,16 @@ class ParseStage(object):
         elif self.debugSyntax == 1:
             return toks.copy()
         
-        # toks is nested like this [["-","5"]]
+        # toks is structured like this [["-","5"]]
         newToks = ParseResults([{"type":"mopP1", "loc":loc}]) #create dict
         newToks += toks[0].copy() #add original contents
         return ParseResults([newToks]) #wrap in []; return.    
     
+    
     def actionMemAccess(self, str, loc, toks):
         """
-        Parse action: Put additional information into parse result, that would 
+        Parse action for memory access (aa.bb.cc). 
+        Put additional information into parse result, that would 
         be lost otherwise. The information is stored in a dict, and put before 
         the original parse result.
         """
@@ -128,12 +141,68 @@ class ParseStage(object):
         elif self.debugSyntax == 1:
             return toks.copy()
         
-        # toks is nested like this [["-","5"]]
+        # toks is structured like this [["-","5"]]
         newToks = ParseResults([{"type":"memA", "loc":loc}]) #create dict
         newToks += toks[0].copy() #add original contents
         return ParseResults([newToks]) #wrap in []; return.    
     
-##    ------------------------------ BNF ------------------------------------
+    
+    def actionFuncCall(self, str, loc, toks):
+        """
+        Parse action for function call.
+        Put additional information into parse result, that would 
+        be lost otherwise. The information is stored in a dict, and put before 
+        the original parse result.
+        """
+        #debug code-----------------
+        if   self.debugSyntax == 2:
+            return None
+        elif self.debugSyntax == 1:
+            return toks.copy()
+        
+        # toks is structured like this [['sin','(',['2'],')']]
+        newToks = ParseResults([{"type":"funcCall", "loc":loc}]) #create dict
+        newToks += toks[0].copy() #add original contents
+        return ParseResults([newToks]) #wrap in []; return.    
+    
+    
+    def actionParentheses(self, str, loc, toks):
+        """
+        Parse action for pair of parentheses: ( 1+2 ).
+        Put additional information into parse result, that would 
+        be lost otherwise. The information is stored in a dict, and put before 
+        the original parse result.
+        """
+        #debug code-----------------
+        if   self.debugSyntax == 2:
+            return None
+        elif self.debugSyntax == 1:
+            return toks.copy()
+        
+        #toks is structured like this [['(', ['1', '+', '2'], ')']]
+        newToks = ParseResults([{"type":"paren", "loc":loc}]) #create dict
+        newToks += toks[0].copy() #add original contents
+        return ParseResults([newToks]) #wrap in []; return.    
+    
+    
+    def actionCheckIdentifier(self, str, loc, toks):
+        """
+        Parse action to check an identifier.
+        Tries to see wether it is equal to a keyword. 
+        Does not change any parse results
+        """
+##        #debug code-----------------
+##        if   self.debugSyntax == 2:
+##            return
+        #toks is structured like this: ['a1']
+        if toks[0] in self.keywords:
+            #print "found keyword", toks[0], "at loc: ", loc
+            #raise ParseException(str, loc, "Identifier same as keyword: %s" % toks[0])
+            raise ParseFatalException(  
+                str, loc, "Identifier same as keyword: '%s'" % toks[0] )
+    
+    
+#------------------- BNF --------------------------------------------------------*
     def defineLanguageSyntax(self):
         """
         Here is Siml's BNF
@@ -145,7 +214,7 @@ class ParseStage(object):
         kw = self.defineKeyword
         
         #Constants that are built into the language
-        #TODO come up with better name: time is no constant
+        #TODO: come up with better name: time is no constant
         builtInConstant = ( kw("e") | kw("pi") | kw("time"))        .setName("builtInConstant")#.setDebug(True)
         
         #Functions that are built into the language
@@ -153,28 +222,31 @@ class ParseStage(object):
                             kw("sqrt") | kw("ln")               )   .setName("builtInFuntion")#.setDebug(True)
         
         #Floating point number.
-        e = CaselessLiteral( "E" )
+        eE = CaselessLiteral( "E" )
         number = ( Combine( 
                     Word(nums) + 
                     Optional("." + Optional(Word(nums))) +
-                    Optional(e + Word("+-"+nums, nums))     )
+                    Optional(eE + Word("+-"+nums, nums))     )
                  )                                                  .setName("builtInConstant")#.setDebug(True)
         
-        #Mathematical expressions ----------------------------------------------------------------------------
+        # .............. Mathematical expression .............................................................
         #"Forward declarations" for recursive rules
         expression = Forward()
         term =  Forward()
         factor = Forward()
         signedAtom = Forward()
-        memAccess = Forward()
+        memAccess = Forward() #For PDE: may also contain expressions for slices: a.b.c(2.5:3.5)
         
-        #Basic building blocks of mathematical expressions (1, x, e, sin(2*a), (a+2))
+        #Basic building blocks of mathematical expressions e.g.: (1, x, e,  
+        #sin(2*a), (a+2), a.b.c(2.5:3.5))
         #Function call, parenthesis and memory access can however contain 
         #independent expressions.
-        funcCall = Group( builtInFuntion + "(" + expression + ")") .setName("funcCall")#.setDebug(True)
-        parenthesis = Group("(" + expression + ")")                .setName("parenthesis")#.setDebug(True)
+        funcCall = Group( builtInFuntion + "(" + expression + ")")  .setParseAction(self.actionFuncCall) \
+                                                                    .setName("funcCall")#.setDebug(True)
+        parentheses = Group("(" + expression + ")")                 .setParseAction(self.actionParentheses) \
+                                                                    .setName("parentheses")#.setDebug(True)
         atom = (    number | builtInConstant | funcCall | 
-                    memAccess | parenthesis               )        .setName("atom")#.setDebug(True)
+                    memAccess | parentheses               )         .setName("atom")#.setDebug(True)
         
         #The basic mathematical operations (-a+b*c^d):
         #All operations have right-to-left associativity; althoug this is only 
@@ -204,12 +276,13 @@ class ParseStage(object):
                                                         .setName("expression2")#.setDebug(True)
         expression << (expression2 | expression1)       .setName("expression")#.setDebug(True)
         
-        #TODO missing are relational operators (<, >, ==, ...).
+        #TODO: missing are relational operators (<, >, ==, ...).
         #They have the lowest precedence and would be placed here
-        #End mathematical expressions ------------------------------------------------------------------------
+        
+        #................ End mathematical expression ................................................---
         
         #Identifiers
-        #TODO: check for keywods
+        #TODO: check for keywods -  .setParseAction(self.actionCheckIdentifier) \
         identifier = Word(alphas, alphas+nums+"_")              .setName("identifier")#.setDebug(True)
         
         #Compound identifiers for variables or parameters "aaa.bbb". 
@@ -220,7 +293,8 @@ class ParseStage(object):
                             ZeroOrMore(dotSup  + identifier) )  .setParseAction(self.actionMemAccess) \
                                                                 .setName("memAccess")#.setDebug(True)
         
-        #End of language definition --------------------------------------------------------------------------
+        #................ End of language definition ..................................................
+        
         #determine start symbol 
         startSymbol = expression
         #set up comments
@@ -230,12 +304,67 @@ class ParseStage(object):
         startSymbol.ignore(singleLineCommentPy)
         
         return startSymbol
-        
+    
+    
     def parseString(self, inString):
         """Parse a whole program. The program is entered as a string."""
         result = self._parser.parseString(inString)
         return result
     
+
+
+
+class Node(object):
+    """Building block of a n-ary tree structure."""
+    
+    def __init__(self, type):
+        self.type = type
+        self.children = []
+
+	def __getitem__(self, i):
+		return self.children[i]
+	def __len__(self):
+		return len(self.children)
+	def __setslice__(self, low, high, seq):
+		self.children[low:high] = seq
+	def __cmp__(self, o):
+		return cmp(self.type, o)
+
+
+
+class Struct(object):
+    """
+    Act like a Matlab struct; store anyhting in tree like way.
+    The object is currently only useful to store numbers, "array" objects  
+    and other "Struct" objects.
+    
+    Supports (recursive) deep copy with copy() function.
+    
+    TODO: write constructor (__init__) that takes the output of __repr__ as input.
+    """
+    
+    def copy(self):
+        """
+        Make a (recursive) deep copy of the object.
+        This will (currently) not work with attributes that are lists or 
+        dictionaries! 
+        See: http://www.python.org/search/hypermail/python-1993/0267.html
+        """
+        newObject = self.__class__()     #Create new object with same class
+        #duplicate attributes
+        for key in self.__dict__.keys(): #key is a string
+            oldAttr = getattr(self, key) 
+            if hasattr(oldAttr, "copy"): #If attribute has a copy function then
+                newAttr = oldAttr.copy() #use the copy function to duplicate it
+            else:
+                newAttr = oldAttr #else shallow copy - attribute is believed to 
+                                  #be immutable e.g.: number.
+            setattr(newObject, key, newAttr) #Put duplicated attribute into 
+                                             #new object
+        return newObject
+
+
+
 
 if __name__ == '__main__':
     # Self-testing code goes here.
@@ -252,7 +381,7 @@ if __name__ == '__main__':
     print parser.parseString("0*1^2*3*4");
     print parser.parseString("0+(1+2)+3+4");
     print parser.parseString("-0+1+--2*-3--4");
-    print parser.parseString("-aa.a+bb.b+--cc.c*-dd.d--ee.e");
+    print parser.parseString("-aa.a+bb.b+--cc.c*-dd.d--ee.e+f");
     print parser.parseString("0+sin(2+3*4)+5");
     print parser.parseString("0+a1.a2+bb.b1.b2+3+4 #comment");
     print parser.parseString("0.123+1.2e3");
