10c4f57dd8c11edc1266cafee3a6583f7f38462b
eike <>
2007-01-11 05:20:27 +0000

    - wrote setup script - wrote real README file

--- freeode_py/setup.py
+++ freeode_py/setup.py
@@ -19,4 +19,51 @@
 ############################################################################
 
 
-#TODO: write setup script
+# Setup script for the freeode simulation software
+#
+# Install the software by issuing on the command line:
+#    python setup.py install
+#
+# Commandline for creating distributions:
+#    python setup.py sdist bdist_rpm bdist_wininst
+#
+# Small overview of frequently used commands. The command line
+# generally is:
+#    python setup.py <command>  [--dry-run]
+#
+# Some commands are:
+#    sdist         : create source distribution. (*.tar.gz or *.zip)
+#    bdist_wininst : create Windows executable installer (binary distribution). (*.exe)
+#    bdist_rpm     : create binary RPM disribution (*.rpm)
+#    install       : install the software
+#    --dry-run     : test the operation without doing anything
+#
+# also usefull:
+#    python setup.py --help
+#    python setup.py --help-commands
+
+
+# IMPORTANT: 
+# Files for the source distribution are also specified in the file:
+#    MANIFEST.in
+
+
+from distutils.core import setup
+from freeode.ast import progVersion
+
+setup(name = 'freeode',
+      version = progVersion,
+      author = 'Eike Welk',
+      author_email = 'Eike.Welk@gmx.net',
+      url = 'http://freeode.berlios.de/',
+      description = 'Simulation Language for Differential Equations',
+      long_description = \
+'''This packge contains a compiler for a specialized language (SIML) for the 
+simulation of differential equations (ODE). The generated simulation program 
+is in the Python programing language, using the Numpy and Scipy libraries.''',
+      license = 'GPL',
+      packages = ['freeode'], 
+      scripts = ['simlc'],
+      #data_files = [('freeode', ['hl_siml.xml'])], # (<folder>, [<file>])
+      )
+
