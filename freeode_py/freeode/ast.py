531b4a3074110dbd908a662cfc1178dbd5952b39
eike <>
2007-01-23 01:38:40 +0000

    minimal doc change

--- freeode_py/freeode/ast.py
+++ freeode_py/freeode/ast.py
@@ -2,6 +2,8 @@
 #    Copyright (C) 2006 by Eike Welk                                       #
 #    eike.welk@post.rwth-aachen.de                                         #
 #                                                                          #
+#    License: GPL                                                         #
+#                                                                          #
 #    This program is free software; you can redistribute it and#or modify  #
 #    it under the terms of the GNU General Public License as published by  #
 #    the Free Software Foundation; either version 2 of the License, or     #
