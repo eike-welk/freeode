0659daf37de61ae920040f7e96f6a5cd841766c5
eike <>
2007-02-09 02:32:44 +0000

    did a little writing

--- website/installation.php
+++ website/installation.php
@@ -23,74 +23,110 @@
   <h1>Installation</h1>
   <h2>Dependencies</h2>
   <p>
-    You need the Python language, and libraries below.
+    You need the
+    <A href="http://www.python.org/">Python</A>
+    language and some libraries:
+    The compiler uses the
+    <A href="http://pyparsing.wikispaces.com/">Pyparsing</A>
+    library.
+    The generated Python program needs the
+    <A href="http://numpy.scipy.org/">NumPy</A>
+    ,
+    <A href="http://www.scipy.org/">SciPy</A>
+    and
+    <A href="http://matplotlib.sourceforge.net/">Matplotlib</A>
+    libraries for numerical computations and plotting.
+  </p>
+
+  <p>
+  Here is a table of the dependencies:
   </p>
   <p>
     <table>
       <tbody>
         <tr>
-          <td><A href="http://www.boost.org/">Boost</A></td>
-          <td>&nbsp;&nbsp;C++ lib</td>
-          <td>&nbsp;&nbsp;usually present on Linux</td>
+          <td>Name</td>
+          <td>What</td>
+          <td>Linux</td>
+          <td>Windows</td>
         </tr>
+
         <tr>
           <td><A href="http://www.python.org">Python</A></td>
-          <td>&nbsp;&nbsp;Programming language</td>
-          <td>&nbsp;&nbsp;usually present on Linux</td>
+          <td>programming language</td>
+          <td>usually already installed</td>
+          <td>special binary distribution for scientist availlable</td>
         </tr>
+
         <tr>
-          <td><A href="http://numeric.scipy.org/">NumPy</A></td>
-          <td>&nbsp;&nbsp;Python lib</td>
-          <td>&nbsp;&nbsp;often present on Linux</td>
+          <td><A href="http://pyparsing.wikispaces.com/">pyparsing</A></td>
+          <td>library for parsers</td>
+          <td>only sources</td>
+          <td>only sources</td>
         </tr>
+
+        <tr>
+          <td><A href="http://numpy.scipy.org/">NumPy</A></td>
+          <td>array object, linear algebra</td>
+          <td>binary packages for <strong>most</strong> distributions</td>
+          <td>comes with scientific Python distribution</td>
+        </tr>
+
         <tr>
           <td><A href="http://www.scipy.org/">SciPy</A></td>
-          <td>&nbsp;&nbsp;Python lib</td>
-          <td></td>
+          <td>scientific algorithms</td>
+          <td>binary packages for <strong>some</strong> distributions</td>
+          <td>comes with scientific Python distribution</td>
         </tr>
+
         <tr>
-          <td><A href="http://gnuplot-py.sourceforge.net/">Gnuplot.py</A></td>
-          <td>&nbsp;&nbsp;Python lib</td>
-          <td></td>
+          <td><A href="http://matplotlib.sourceforge.net/">Matplotlib</A></td>
+          <td>plotting</td>
+          <td>binary packages for <strong>some</strong> distributions</td>
+          <td>comes with scientific Python distribution</td>
         </tr>
       </tbody>
     </table>
   </p>
 
-  <h2>Linux</h2>
-  <h3>Compiling from sources</h3>
-  <p>TODO: create source package.</p>
+
+  <h2>Installing Dependencies</h2>
+  <h3>Python</h3>
+  <h3>Pyparsing</h3>
+  <h3>Numpy</h3>
+  <h3>SciPy</h3>
+  <h3>Matplotlib</h3>
+
+
+  <h2>Installing Freeode</h2>
+  <p>The installation of the freeode package is very easy:</p>
   <p>
-    Alternatively you could download the sources with subversion. This is explained in
-    the <A href="development.html">development</A> section.
-  </p>
-
-  <h3>Binary Package</h3>
-  <p>
-    If you don't feel like compiling the program you can download a binary version.
-    It was compiled on a Suse 9.3 system with <strong>gcc version 3.3.5</strong>.
-
-    <ul>
-      <li>
-        Download the file 'siml-binary-0.2.0.tar.gz' from the
-        <A href="https://developer.berlios.de/project/showfiles.php?group_id=5610">download</A>
-        section
-      </li>
-      <li>Extract the executable from the archive. This results in a file named 'siml'</li>
-      <li>Copy the executable to a directory in your PATH. Usually: '/usr/local/bin'</li>
-      <li>Use the compiler: siml filename.siml</li>
-    </ul>
-  </p>
-
-  <h2>Windows</h2>
-  <p>Currently there is no Windows version, although porting should be easy.</p>
-
-  <h2>Examples</h2>
-  <p>
-    There is also a package with example files in the
+    Get the latest source <strong>tar.gz</strong> package
+    (for example <strong>freeode-0.3.0.tar.gz</strong>) from the
     <A href="https://developer.berlios.de/project/showfiles.php?group_id=5610">download</A>
-    section: 'models.tar.gz'.
+    section and and extract the contents.
+    Enter the generated directory and run the setup script (<strong>python setup.py install</strong>).
+    Ready.
   </p>
+  <p>
+    You can also create binary packages (<strong>rpm deb? exe</strong>) with this script;
+    to install them with the operating system's software management tool.
+    Look at the comments inside the <strong>setup.py</strong> script.
+  </p>
+  </p>
+  <h4>Linux</h4>
+  tar xvf ....
+  <h4>Windows</h4>
+  <p>
+    Extract the contents of the
+    <A href="https://developer.berlios.de/project/showfiles.php?group_id=5610">downloaded</A>
+    <strong>tar.gz</strong> file.
+    You can for example use the
+    <A href="http://www.winzip.com/">Winzip</A>
+    program.
+  </p>
+  <p>TODO: see how other projects give generic installation instructions.</p>
+
 </div>
 
 
