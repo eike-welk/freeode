cc60e96040fe1e4edcbe9f86a823efe106af420c
eike <>
2008-04-24 21:38:28 +0000

    add this hack for an additional dot operator

---
+++ freeode_py/freeode/test_dot_operator.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#    Copyright (c) 2008  Eike Welk                                          #
+#    eike.welk@post.rwth-aachen.de                                          #
+#                                                                           #
+#    License: Expat License (X11 License)                                   #
+#                                                                           #
+#    Permission is hereby granted, free of charge, to any person obtaining  #
+#    a copy of this software and associated documentation files (the        #
+#    "Software"), to deal in the Software without restriction, including    #
+#    without limitation the rights to use, copy, modify, merge, publish,    #
+#    distribute, sublicense, and/or sell copies of the Software, and to     #
+#    permit persons to whom the Software is furnished to do so, subject to  #
+#    the following conditions:                                              #
+#                                                                           #
+#    The above copyright notice and this permission notice shall be         #
+#    included in all copies or substantial portions of the Software.        #
+#                                                                           #
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        #
+#    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     #
+#    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. #
+#    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   #
+#    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   #
+#    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      #
+#    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 #
+#############################################################################
+
+'''
+Hack, to permit a syntax that looks like an additional operator for the dot 
+product would exist:
+
+    c = a *dot* b
+
+(The idea is from some Python website; maybe cheese shop.)
+'''
+
+from __future__ import division
+
+import numpy as np
+from numpy import dot as np_dot
+from numpy import ndarray
+
+
+
+class DotHelper1(object):
+    '''Helper class for the dot pseudo operator'''
+    __array_priority__ = 10.0
+    
+    def __init__(self):
+        pass
+    
+    def __call__(self, dim=None):
+        '''Might be used to pass the dimension over which the dot product is taken'''
+        print '__call__ is not yet implemented.'
+        return DotHelper1()
+    
+    def __rmul__(self, leftOP):
+        if not isinstance(leftOP, ndarray):
+            return NotImplemented
+        return DotHelper2(leftOP)
+
+
+class DotHelper2(object):
+    '''Helper class for the dot pseudo operator'''
+    
+    def __init__(self, operand=None):
+        self.operand = operand
+    
+    def __mul__(self, rightOp):
+        if not isinstance(rightOp, np.ndarray):
+            return NotImplemented
+        if self.operand is None:
+            raise Exception('Unknown left operand!')
+        return np_dot(self.operand, rightOp)
+    
+    
+dot = DotHelper1()
+
+
+if __name__ == '__main__':
+    from numpy import array, linspace, identity
+    
+    a = array([1.0, 2, 3])
+    b = array([4.0, 5, 6])
+    m = linspace(1, 9, 9).reshape((3,3))
+    id = identity(3, float)
+    
+    print 'a: ', a 
+    print 'b: ', b 
+    print 'm: ', m 
+    print
+    
+    #print a *dot(2)* b 
+    print 'a *dot* b         : ', a *dot* b 
+    print 'm *dot* a         : ', m *dot* a
+    print 'a *dot* m         : ', a *dot* m
+    print 'a *dot* id *dot* a: ', a *dot* id *dot* a
+ 
