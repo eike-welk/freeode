48180279b04652903ca398ebc8df95439fee88af
eike <>
2009-11-29 23:54:27 +0000

    Wrote untested parser tnat check the indent level, and that use the trick of using float values for the loc.

--- freeode_py/experiments/indent_scanner.py
+++ freeode_py/experiments/indent_scanner.py
@@ -16,82 +16,184 @@ decreases by two levels:
 
 A possible idea to make packrat parsing compatible with the outdent parser is
 to use floating point values for the current location of the parser.
+
+FIXME: The current implementation does not function with comments. 
 '''
 
 
 import re
+import pyparsing
 
 
-class IndentScanner(object):
-    def __init__(self):
-        pass
+def compute_indent_line_end_list(input_text):
+    '''
+    Scan through text and compute indent width and position of newline,
+    for each line that contains text. 
     
-    def computeIndentLineEndList(self, input_text):
-        '''
-        Scan through text and compute indent width and position of newline,
-        for each line that contains text. 
+    Argument:
+    ---------
+    input_text:
+        Text for which indent widths are computed
         
-        Argument:
-        ---------
-        input_text:
-            Text for which indent widths are computed
-            
-        Return:
-        -------
-        list(tuple(ini, int)):
-            Each tuple is: (indent width, position of newline)   
-        '''
-        black_finder = re.compile(r'[\S]') #matches any non-whitespace char
-        indent_line_end = [] #function result
-        line_start, line_end = 0, None
-        while line_start < len(input_text):
-            #find end of current line - search for next newline character
-            #if no newline is found, current line extends until end of text
-            line_end = input_text.find('\n', line_start)
-            if line_end == -1:
-                line_end = len(input_text)
-            #find position of first black character in current line
-            #compute indent width from this position 
-            black = black_finder.search(input_text, line_start, line_end)
-            if black:
-                indent_width = black.start() - line_start
-                indent_line_end.append((indent_width, line_end))
-            #advance to next line
-            line_start = line_end + 1
-        return indent_line_end
+    Return:
+    -------
+    list(tuple(ini, int)):
+        Each tuple is: (indent width, position of newline)   
+    '''
+    black_finder = re.compile(r'[\S]') #matches any non-whitespace char
+    indent_line_end = [] #function result
+    line_start, line_end = 0, None
+    while line_start < len(input_text):
+        #find end of current line - search for next newline character
+        #if no newline is found, current line extends until end of text
+        line_end = input_text.find('\n', line_start)
+        if line_end == -1:
+            line_end = len(input_text)
+        #find position of first black character in current line
+        #compute indent width from this position 
+        black = black_finder.search(input_text, line_start, line_end)
+        if black:
+            indent_width = black.start() - line_start
+            indent_line_end.append((indent_width, line_end))
+        #advance to next line
+        line_start = line_end + 1
+    return indent_line_end
+
+
+def compute_indent_outdent_maps(indents_line_ends, input_text):
+    '''
+    Find line endings where the indent width changes.
     
-    
-    def findIndentOutdentTags(self, indent_line_end):
-        '''
-        Iterate through the list of indent width and line-end positions
-        and generate indent and outdent tags.
-        '''
-        indent_tags, outdent_tags = {}, {}
-        indent_stack = [0]
-        for indent_width, line_end_pos in indent_line_end:
-            #no change of indent
-            if indent_width == indent_stack[-1]:
-                continue
-            #indent increases, generate indent tag
-            elif indent_width > indent_stack[-1]:
-                indent_stack.append(indent_width)
-                indent_tags[line_end_pos] = indent_width
-            #indent decreases, generate outdent tag
+    For each newline where the indent depth changes, create 
+    an entry in a map: position in text -> indent width.
+    Two maps are maintained, one where the indent increases (indent)
+    and an other where the indent decreases (outdent).
+    '''
+    indent_tags, outdent_tags = {}, {}
+    indent_stack = [0]
+    il_it = indents_line_ends.__iter__()
+    old_width, old_pos = il_it.next() 
+    if old_width != 0:
+        raise Exception('Indentation width at beginning of file must be 0!')
+    for indent_width, line_end_pos in il_it:
+        #no change of indent
+        if indent_width == indent_stack[-1]:
+            pass
+        #indent increases, generate indent tag
+        elif indent_width > indent_stack[-1]:
+            indent_stack.append(indent_width)
+            indent_tags[old_pos] = indent_width
+        #indent decreases, generate outdent tag
+        elif indent_width < indent_stack[-1]:
             #the outdent can cover multiple steps
-            elif indent_width < indent_stack[-1]:
-                while indent_width < indent_stack[-1]:
-                    indent_stack.pop()
-                if indent_stack[-1] != indent_width:
-                    raise Exception("inconsistent indent!")
-                outdent_tags[line_end_pos] = indent_width
-            #data is corrupted
-            else:
-                raise Exception('Scanner internal error.')
-            
-        return indent_tags, outdent_tags    
+            while indent_width < indent_stack[-1]:
+                indent_stack.pop()
+            if indent_stack[-1] != indent_width:
+                line_no = pyparsing.lineno(line_end_pos, input_text)
+                raise Exception("Inconsistent indent! "
+                                "Line: %d, offending indent width: %d"
+                                % (line_no, indent_width))
+            outdent_tags[old_pos] = indent_width
+        #data is corrupted
+        else:
+            raise Exception('Scanner internal error.')
+        #remember position from previous line
+        old_pos = line_end_pos
+    return indent_tags, outdent_tags    
             
                          
+class IndentData(object):
+    '''Data to coordinate the indent parsers'''
+    def __init__(self):
+        self.indent_map = {}
+        self.outdent_map = {}
+        self.indent_stack = [0]
+        self.init = False
+        
+    def initialize_maps(self, input_text):
+        """Create the maps that identify indents and outdents"""
+        indent_list = compute_indent_line_end_list(input_text)
+        self.indent_map, self.outdent_map = \
+                    compute_indent_outdent_maps(indent_list, input_text)
+        self.init = True
+        
 
+class LineEndIndent(pyparsing.LineEnd):
+    '''
+    Parse end of line and increasing indent (in next line).
+    '''    
+    def __init__(self, data):
+        pyparsing.LineEnd.__init__(self)
+        self.data = data
+        
+    def parseImpl( self, instring, loc, doActions=True ):
+        '''Do the parsing work'''
+        #Initialize the maps if necessary
+        if not self.data.init:
+            self.data.initialize_maps()
+        #test if there is a newline at loc
+        new_loc, char = pyparsing.LineEnd.parseImpl(self, instring, 
+                                                    int(loc), doActions)
+        #test if there is also an increased indent at the next line
+        indent_width = self.data.indent_map.get(int(loc), None)
+        if indent_width:
+            self.data.indent_stack.append(indent_width)
+            return new_loc, char
+        else:
+            raise pyparsing.ParseException(instring, loc, "Expecting increasing indent.", self)
+    
+    
+class LineEndNodent(pyparsing.LineEnd):
+    '''
+    Parse end of line and constant indent.
+    '''    
+    def __init__(self, data):
+        pyparsing.LineEnd.__init__(self)
+        self.data = data
+        
+    def parseImpl( self, instring, loc, doActions=True ):
+        '''Do the parsing work'''
+        #test if there is a newline at loc
+        new_loc, char = pyparsing.LineEnd.parseImpl(self, instring, 
+                                                    int(loc), doActions)
+        #test if indent is changing at the next line
+        i_loc = int(loc)
+        if not (i_loc in self.data.indent_map or 
+                i_loc in self.data.outdent_map   ): 
+            return new_loc, char
+        else:
+            raise pyparsing.ParseException(instring, loc, "Expecting constant indent.", self)
+    
+    
+class LineEndOutdent(pyparsing.LineEnd):
+    '''
+    Parse end of line and decreasing indent (in next line).
+    '''    
+    def __init__(self, data):
+        pyparsing.LineEnd.__init__(self)
+        self.data = data
+        
+    def parseImpl( self, instring, loc, doActions=True ):
+        '''Do the parsing work'''
+        #test if there is a newline at loc
+        new_loc, char = pyparsing.LineEnd.parseImpl(self, instring, 
+                                                    int(loc), doActions)
+        #test if indent is decreasing at the next line
+        indent_width = self.data.outdent_map.get(int(loc), None)
+        if indent_width: 
+            self.data.indent_stack.pop()
+            #if multiple un-indents happen at this character, 
+            #don't go to the next character, but increase loc only a tiny bit
+            #return a float as loc
+            if self.data.indent_stack[-1] < indent_width:
+                return loc + 1./indent_width, char
+            else:
+                return new_loc, char
+        else:
+            raise pyparsing.ParseException(instring, loc, "Expecting decreasing indent.", self)
+    
+    
+    
 if __name__ == '__main__':
     #correct example
     input_text = """\
@@ -102,10 +204,9 @@ if __name__ == '__main__':
   2
 0
 """
-    scan = IndentScanner()
-    il = scan.computeIndentLineEndList(input_text)
+    il = compute_indent_line_end_list(input_text)
     print il
-    intags, outtags = scan.findIndentOutdentTags(il)
+    intags, outtags = compute_indent_outdent_maps(il, input_text)
     print intags
     print outtags
     
