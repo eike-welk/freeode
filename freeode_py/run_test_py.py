6f5fc8640627a2e1aa6530b693965f4a07fd78a6
Eike Welk <Eike.Welk@gmx.net>
2010-05-16 23:41:31 +0200

    Added test script runner for convenience.

---
+++ freeode_py/run_test_py.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#Run all tests with "py.test"
+#"py.test" is a program to find tests and to run them.
+
+import os
+#run tests on 2 CPUs and produce only short back trace.
+os.system("py.test -n 2 --tb=short")
